0949e9a9a61aaf78ed1e54da5d026330461c9303
AndrewRotolo <rotolo202@gmail.com>
2023-06-09 01:00:39 -0400

    react app set up + BASIC header stuff

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Header() {
+
+    return(
+        <div className='container'>
+            <h1 id='title'>Andrew Rotolo</h1>
+        <p>
+            <a href='' id='link1' className='links'>About Me</a>
+            <a href='' id='link2' className='links'>Portfolio</a>
+            <a href='' id='link3' className='links'>Contact</a>
+            <a href='' id='link4' className='links'>Resume</a>
+        </p>
+        </div>
+    )
+}
